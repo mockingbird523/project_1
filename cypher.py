3483ec29c30a31c1c60624396a24e74d35e7a460
mockingbird523 <anncleary523@gmail.com>
2025-04-27 18:17:21 +0100

    First part of Caesar cipher

---
+++ cypher.py
@@ -0,0 +1,37 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+
+def encrypt(original_text, shift_amount):
+    encrypted_letters = ""
+    for letter in original_text:
+       shifted_position = alphabet.index(letter)+shift_amount
+       shifted_position = shifted_position % 26
+       encrypted_letters+=alphabet[shifted_position]
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
